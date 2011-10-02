1a63d491a06587ea5bcaf97bc73040cc0b4672c5
Lonnen <chris.lonnen@gmail.com>
2011-10-02 01:40:17 -0700

    updating projects

--- media/script.js
+++ media/script.js
@@ -16,7 +16,7 @@ makeScript(src);
 
 
 /** Github projects **/
-var names = ['socorro','scottbot','processing-js','Introspecting'];
+var names = ['socorro','socorro-vagrant','scottbot','processing-js','Introspecting','timbermill'];
 var project_el = document.querySelector('#projects')
 if (project_el) {
     var github = 'http://github.com/api/v1/json/lonnen/';
