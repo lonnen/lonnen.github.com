b729cf6c0ffaac0ad50f29cd17543daaad569731
Lonnen <chris.lonnen@gmail.com>
2012-08-20 17:15:27 -0700

    added gaug.es

--- media/script.js
+++ media/script.js
@@ -14,6 +14,19 @@ var makeScript = function(src) {
 var src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 makeScript(src);
 
+// start gaug.es
+var _gauges = _gauges || [];
+(function() {
+    var t   = document.createElement('script');
+    t.type  = 'text/javascript';
+    t.async = true;
+    t.id    = 'gauges-tracker';
+    t.setAttribute('data-site-id', '5032d20cf5a1f50d4e000061');
+    t.src = '//secure.gaug.es/track.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(t, s);
+})();
+// end
 
 /** Github projects **/
 var names = ['socorro','socorro-vagrant','scottbot','processing-js','Introspecting','timbermill'];
